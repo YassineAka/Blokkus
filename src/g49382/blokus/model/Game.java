8d1e4c4eadfcddf6a1746addfa5271bee1066943
49382 <49382@etu.he2b.be>
2018-12-17 02:00:41 +0100

    remain newgame,javadoc and test...

--- src/g49382/blokus/model/Game.java
+++ src/g49382/blokus/model/Game.java
@@ -58,6 +58,7 @@ public class Game extends Observable {
     public boolean isOver() {
         if (players.isEmpty()) {
             this.isOver = true;
+            
         }
         return isOver;
     }
